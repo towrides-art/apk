94b1632fdba1ae22b8f06d4fcefb73f046ef7ea9
Not-deadx <notdeadtech09@gmail.com>
2026-05-10 23:11:30 +0530

    Final Firebase removal - all files cleaned

--- ios/TowRides/AppDelegate.swift
+++ ios/TowRides/AppDelegate.swift
@@ -2,8 +2,6 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import FirebaseCore
-import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
