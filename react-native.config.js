562ba944f7cb1a50e40dcecc439b2d85cf21c1f4
Not-deadx <notdeadtech09@gmail.com>
2026-05-11 21:27:34 +0530

    iOS: Disable Firebase autolinking and add post_install pod removal

--- react-native.config.js
+++ react-native.config.js
@@ -15,5 +15,20 @@ module.exports = {
         ios: null,
       },
     },
+    '@react-native-firebase/app': {
+      platforms: {
+        ios: null,
+      },
+    },
+    '@react-native-firebase/messaging': {
+      platforms: {
+        ios: null,
+      },
+    },
+    '@react-native-firebase/auth': {
+      platforms: {
+        ios: null,
+      },
+    },
   },
 };
