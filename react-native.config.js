9891cb5486064a887166229adbd38aaadeeee6f6
Not-deadx <notdeadtech09@gmail.com>
2026-05-12 12:09:19 +0530

    FINAL CLEAN: cache disabled + nuclear Firebase removal at each step + Android autolink disabled

--- react-native.config.js
+++ react-native.config.js
@@ -18,16 +18,19 @@ module.exports = {
     '@react-native-firebase/app': {
       platforms: {
         ios: null,
+        android: null,
       },
     },
     '@react-native-firebase/messaging': {
       platforms: {
         ios: null,
+        android: null,
       },
     },
     '@react-native-firebase/auth': {
       platforms: {
         ios: null,
+        android: null,
       },
     },
   },
