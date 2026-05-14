c3523792d753e9186e24f5090c9094fe3b142c87
Not-deadx <notdeadtech09@gmail.com>
2026-05-14 10:54:45 +0530

    fix: MUST KEEP Firebase autolinking block in react-native.config.js - this prevents pod install from adding RNFB

--- react-native.config.js
+++ react-native.config.js
@@ -15,29 +15,5 @@ module.exports = {
         ios: null,
       },
     },
-    '@react-native-firebase/app': {
-      platforms: {
-        ios: null,
-        android: null,
-      },
-    },
-    '@react-native-firebase/auth': {
-      platforms: {
-        ios: null,
-        android: null,
-      },
-    },
-    '@react-native-firebase/messaging': {
-      platforms: {
-        ios: null,
-        android: null,
-      },
-    },
-    '@notifee/react-native': {
-      platforms: {
-        ios: null,
-        android: null,
-      },
-    },
   },
 };
